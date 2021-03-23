9411093dba8c737b549d3924107ab721f82011ac
Zane Billings <39701545+wz-billings@users.noreply.github.com>
2021-03-23 01:18:55 -0400

    Updated data cleaning script to pull raw text from PG website rather than local storage.

--- data-raw/bardr-data-cleaning.R
+++ data-raw/bardr-data-cleaning.R
@@ -5,6 +5,7 @@
 
 # Import libraries
 ## For wrangling
+library(readr)
 library(dplyr)
 library(here)
 library(magrittr)
@@ -17,65 +18,28 @@ library(tm)
 ## For package stuff
 library(usethis)
 
-# Import the raw text
-raw_text <- readLines(here::here("data-raw", "raw-text.txt"))
-raw_text <- stringi::stri_enc_toascii(raw_text)
-# Extract the list of works from the complete raw text file.
+# Download raw text from project gutenberg, skipping the intro.
+raw_text <- read_lines("https://www.gutenberg.org/files/100/100-0.txt")
+# Get starting and ending lines of the work and remove PG header and footer.
+text_start <- str_which(raw_text, "THE SONNETS")[2] # Use 2 to skip contents
+text_end <- str_which(raw_text, "FINIS")
+work_text <- raw_text[text_start:text_end]
+# Remove any NA lines and blank lines.
+work_text <- work_text[!is.na(work_text)]
+work_text <- work_text[work_text != ""]
+
+
+# List of works copied from the contents. Better to hard code this
+# as the list of works does not change but the lines where the ToC appears has.
 # This will give the "complete"/"long" names for the plays that are
 #  used to label each of the works in the collection.
-work_names <- raw_text %>%
-  extract(43:129) %>%
-  stri_remove_empty() %>%
-  str_trim()
+work_names <- read_lines(here::here("data-raw", "long-names.txt"))
 
 # Manually add the "short"/"common" names for the works.
 # Referenced these from http://shakespeare.mit.edu/.
 # Added in same order as works were listed in the complete text.
 # These are in correct title case so they can be easily used as labels.
-short_name <- c("Sonnets",
-                "All's Well that Ends Well",
-                "Antony and Cleopatra",
-                "As You Like It",
-                "The Comedy of Errors",
-                "Coriolanus",
-                "Cymbeline",
-                "Hamlet",
-                "Henry IV, part 1",
-                "Henry IV, part 2",
-                "Henry V",
-                "Henry VI, part 1",
-                "Henry VI, part 2",
-                "Henry VI, part 3",
-                "Henry VIII",
-                "King John",
-                "Julius Caesar",
-                "King Lear",
-                "Love's Labour's Lost",
-                "Macbeth",
-                "Measure for Measure",
-                "The Merchant of Venice",
-                "The Merry Wives of Windsor",
-                "A Midsummer Night's Dream",
-                "Much Ado About Nothing",
-                "Othello",
-                "Pericles, Prince of Tyre",
-                "Richard II",
-                "Richard III",
-                "Romeo and Juliet",
-                "Taming of the Shrew",
-                "The Tempest",
-                "Timon of Athens",
-                "Titus Andronicus",
-                "Troilus and Cressida",
-                "Twelfth Night",
-                "Two Gentlemen of Verona",
-                "The Two Noble Kinsmen",
-                "Winter's Tale",
-                "A Lover's Complaint",
-                "The Passionate Pilgrim",
-                "The Phoenix and the Turtle",
-                "The Rape of Lucrece",
-                "Venus and Adonis")
+short_names <- read_lines(here::here("data-raw", "short-names.txt"))
 
 # Manually specify the genre for each work.
 # Referenced these from
@@ -99,27 +63,14 @@ for (i in 1:length(genres)) {
 }
 
 # Dataframe of metadata for works
-work_info <- data.frame(work = work_names, short_name, genre = genres)
-
-# Get starting and ending line numbers
-text_start <- str_which(raw_text, "THE SONNETS")[2] # Use 2 to skip contents
-text_end <- str_which(raw_text, "FINIS")
-
-# Save intro and closing, just in case.
-intro <- raw_text[1:text_start - 1]
-outro <- raw_text[text_end:length(raw_text)]
-
-# Export intro and outra
-usethis::use_data(intro, outro, overwrite = TRUE)
-
-# Remove beginning of doc (list of works/project gutenberg info)
-text_trimmed <- raw_text[text_start:text_end - 1]
+work_info <- data.frame(work = work_names, short_names, genre = genres)
 
 # Here I manually add more work names, since part of the play titles
 # in the actual ebook are inconsistent with the table of contents.
 # Pretty sure there is no other way to fix this other than writing
 # a more general regex later, which is more work than this.
-inconsistent_titles <- c("THE LIFE OF KING HENRY V",
+inconsistent_titles <- c("ANTONY AND CLEOPATRA",
+                         "THE LIFE OF KING HENRY V",
                          "MACBETH",
                          " MUCH ADO ABOUT NOTHING",
                          "OTHELLO, THE MOOR OF VENICE",
@@ -129,40 +80,33 @@ inconsistent_titles <- c("THE LIFE OF KING HENRY V",
 
 # The list of allowed titles will be these inconsistent titles, plus the
 # actual titles as specified in the table of contents.
-# Regex works by finding any of these allowed titles on a line by itself.
-# This prevents issues with Cymbeline and King John appearing many times.
-allowed_titles <- c(inconsistent_titles, work_info$work)
+# Unfortunately there is not a good regex solution to this.
+# The best solution would be for PG to make all of the tiles consistent but until
+# that happens this has to be checked every update.
+allowed_titles <- c(inconsistent_titles, str_to_upper(work_names))
 works_pattern <- START %R% or1(allowed_titles) %R% END
-split_indices <- str_which(text_trimmed, pattern = works_pattern)
+split_indices <- str_which(work_text, pattern = works_pattern)
 
 # Venus and Adonis is titled twice. Since Venus and Adonis is the last work,
 # this is easily fixed by overwriting the last index with the end index.
-split_indices[nrow(work_info) + 1] <- length(text_trimmed)
+split_indices[length(split_indices)] <- str_which(work_text, "FINIS")
+
+# Convert to ASCII for CRAN standards
+# Have to do this here so ASCII doesn't mess up REGEX stuff.
+work_text <- stringi::stri_enc_toascii(work_text)
 
 # Make each work a character vector, and put these all into a named list.
 works_list <- vector("list", length(split_indices) - 1)
 names(works_list) <- work_info$short_name
 for (i in 1:(length(split_indices) - 1)) {
   sec_start <- split_indices[i]
-  sec_end <- split_indices[i + 1] - 1
-  works_list[[i]] <- text_trimmed[sec_start:sec_end]
+  sec_end <- split_indices[i + 1] - 1 # -1 to prevent next title inclusion
+  works_list[[i]] <- work_text[sec_start:sec_end]
 }
 
 # Export list of works
 all_works_list <- works_list
 usethis::use_data(all_works_list, overwrite = TRUE)
-
-# Extract each work as its own datafile
-names <- works_list %>%
-  names() %>%
-  tm::removePunctuation() %>%
-  snakecase::to_snake_case()
-
-# Removes each work (as a char vector) from the list and exports.
-for (i in 1:length(works_list)) {
-  this <- assign(names[[i]], works_list[[i]])
-  do.call("use_data", list(as.name(names[[i]]), overwrite = TRUE))
-}
 
 # Intialize blank list for df's
 work_dfs <- vector("list", length(works_list))
@@ -179,7 +123,7 @@ works_combined <- do.call(rbind, work_dfs)
 
 # Join df with values from metadeta LUT and fix names
 works_tidy <- do.call(rbind, work_dfs) %>%
-  inner_join(work_info, by = c("name" = "short_name"))
+  inner_join(work_info, by = c("name" = "short_names"))
 colnames(works_tidy) <- c("name", "content", "full_name", "genre")
 
 all_works_df <- works_tidy
